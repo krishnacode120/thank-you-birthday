0c903a79854a57f32d6536dc64080b8b5ee9d955
krishnamoorthy <krishnamoorthy12b@gmail.com>
2025-06-18 21:30:01 +0530

    🎉 Fix routing for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: true
+  }
 });
