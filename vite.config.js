05e9ab947dd819b658072b8bf9eb3c3d03236904
krishnamoorthy <krishnamoorthy12b@gmail.com>
2025-06-18 21:28:14 +0530

    🚀 Fix GitHub Pages routing with 404 and base config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
 export default defineConfig({
-  base: '/thank-you-birthday/', // 👈 Change this to your GitHub repo name exactly
   plugins: [react()],
 });
